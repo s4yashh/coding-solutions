43e115842de70b02fb9c8c2772e1902e8e6aad00
Suyash <singhsuyash012@gmail.com>
2025-11-24 21:13:22 +0530

    feat:Solve LC Q.17

---
+++ leetcode-solutions/2.Medium/lettercombinationofaphonenumber.cpp
@@ -0,0 +1,41 @@
+// question link: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+class Solution
+{
+public:
+    vector<string> letterCombinations(string digits)
+    {
+        if (digits.empty())
+            return {};
+
+        vector<string> mapping = {
+            "", "", "abc", "def", "ghi", "jkl",
+            "mno", "pqrs", "tuv", "wxyz"};
+
+        vector<string> result;
+        string current;
+
+        backtrack(0, digits, mapping, current, result);
+        return result;
+    }
+
+private:
+    void backtrack(int index, string &digits, vector<string> &mapping,
+                   string &current, vector<string> &result)
+    {
+
+        if (index == digits.size())
+        {
+            result.push_back(current);
+            return;
+        }
+
+        string letters = mapping[digits[index] - '0'];
+
+        for (char c : letters)
+        {
+            current.push_back(c);
+            backtrack(index + 1, digits, mapping, current, result);
+            current.pop_back(); // backtrack
+        }
+    }
+};
