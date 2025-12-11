0ea738d878bfd7b1bb465f84c13663375528b7b2
Suyash <singhsuyash012@gmail.com>
2025-12-11 20:35:37 +0530

    feat:Solve Q.54 Spiral matrix

---
+++ leetcode-solutions/2.Medium/spiralmatrix.cpp
@@ -0,0 +1,46 @@
+class Solution
+{
+public:
+    vector<int> spiralOrder(vector<vector<int>> &matrix)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> result(m * n);
+        int k = 0;
+
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
+
+        while (top <= bottom && left <= right)
+        {
+
+            // left to right
+            for (int i = left; i <= right; i++)
+                result[k++] = matrix[top][i];
+            top++;
+
+            // top to bottom
+            for (int i = top; i <= bottom; i++)
+                result[k++] = matrix[i][right];
+            right--;
+
+            // right to left
+            if (top <= bottom)
+            {
+                for (int i = right; i >= left; i--)
+                    result[k++] = matrix[bottom][i];
+                bottom--;
+            }
+
+            // bottom to top
+            if (left <= right)
+            {
+                for (int i = bottom; i >= top; i--)
+                    result[k++] = matrix[i][left];
+                left++;
+            }
+        }
+        return result;
+    }
+};
