555c8677f50c028b5c67fb99e3a6dc4d4a085136
Suyash <singhsuyash012@gmail.com>
2025-12-14 21:29:06 +0530

    feat:Solve Q.86

---
+++ leetcode-solutions/2.Medium/partitionlist.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    ListNode *partition(ListNode *head, int x)
+    {
+        ListNode lessDummy(0), greaterDummy(0);
+        ListNode *less = &lessDummy;
+        ListNode *greater = &greaterDummy;
+
+        while (head != nullptr)
+        {
+            if (head->val < x)
+            {
+                less->next = head;
+                less = less->next;
+            }
+            else
+            {
+                greater->next = head;
+                greater = greater->next;
+            }
+            head = head->next;
+        }
+
+        // important to avoid cycle
+        greater->next = nullptr;
+
+        // join both lists
+        less->next = greaterDummy.next;
+
+        return lessDummy.next;
+    }
+};
