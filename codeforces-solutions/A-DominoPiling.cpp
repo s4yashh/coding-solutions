627a650150066d86f9adb00a1597908a00804ee9
Suyash <singhsuyash012@gmail.com>
2026-01-06 21:55:30 +0530

    feat:Solve problem 50A

---
+++ codeforces-solutions/A-DominoPiling.cpp
@@ -0,0 +1,11 @@
+// codeforces question link: https://codeforces.com/problemset/problem/50/A
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int M, N;
+    cin >> M >> N;
+    cout << (M * N) / 2 << endl;
+    return 0;
+}
